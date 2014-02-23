4fe05f320c8c1ed44066f6e7fcae135c727daefe
Elliot Hutchinson <elliot@elliothutchinson.com>
2014-02-22 21:52:06 -0500

    Implement select folder

--- MRI_Scanner/src/mri_scanner/FXMLDocumentController.java
+++ MRI_Scanner/src/mri_scanner/FXMLDocumentController.java
@@ -2,8 +2,11 @@ package mri_scanner;
 
 
 import analysis.Analyze;
+
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,23 +14,28 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.DirectoryChooser;
 
 /**
  *
  * @author Brian
  */
 public class FXMLDocumentController extends AnchorPane implements Initializable {
-    
-    private Label label;
     private MRIPrototype application;
-    @FXML
-    private Button settings;
-    @FXML
-    private Button selectFolder;
-    @FXML
-    private Button backButton;
+    
     @FXML
     private Label progName;
+    
+    @FXML
+    private Button buttonBack;
+    @FXML
+    private Button buttonSettings;
+    @FXML
+    private Button buttonSelectFolder;
+    @FXML
+    private Button buttonAnalyze;
+    
     @FXML
     private ImageView imageChoose1;
     @FXML
@@ -39,21 +47,48 @@ public class FXMLDocumentController extends AnchorPane implements Initializable 
     @FXML
     private ImageView mainImage;
     
-    private void handleButtonAction(ActionEvent event) {
-	    System.out.println("You clicked me!");
-	    label.setText("Hello World!");
+    private String dir = null;
+    private String sep = File.separator;
+    
+    // TODO Method is called from back button: change button label to "Select" (for temp prototype)
+    // TODO or (preferably) implement ImageView buttons to handle selecting any image for main image
+    public void setImages(ActionEvent event) {
+    	if (dir != null) {
+    		mainImage.setImage(fileManager.setImage(dir + sep + "mri2.jpg"));
+    	}
+    	else {
+    		System.out.println("No directory selected!");
+    	}
+    }
+    
+    public void getImageDirectory(ActionEvent event) {
+    	String initialDir = "user.dir";
+    	
+    	DirectoryChooser directoryChooser = new DirectoryChooser();
+    	directoryChooser.setInitialDirectory(new File(System.getProperty(initialDir)));
+    	directoryChooser.setTitle("Select MRI image directory");
+    	
+    	File file = directoryChooser.showDialog(null);
+    	
+    	// TODO to modify code to work on any number of images
+    	// TODO also check to operate only on images to avoid crash
+    	if (file != null) {
+    		dir = file.getPath();
+    		System.out.println("Loading images in: " + dir);
+    		imageChoose1.setImage(fileManager.setImage(dir + sep + file.listFiles()[0].getName()));
+    		imageChoose2.setImage(fileManager.setImage(dir + sep + file.listFiles()[1].getName()));
+    		imageChoose3.setImage(fileManager.setImage(dir + sep + file.listFiles()[2].getName()));
+    		imageChoose4.setImage(fileManager.setImage(dir + sep + file.listFiles()[3].getName()));
+    	}
     }
         
-    public void setImages(ActionEvent event) {
-		imageChoose1.setImage(fileManager.setImage("MriPics\\Hirnmetastase_MRT-T1_KM.jpg"));
-		imageChoose2.setImage(fileManager.setImage("MriPics\\MRI_T2_Brain_axial_image.jpg"));
-		imageChoose3.setImage(fileManager.setImage("MriPics\\Tumor_BrainstemGlioma2.jpg"));
-		imageChoose4.setImage(fileManager.setImage("MriPics\\mri1.jpg"));
-		mainImage.setImage(fileManager.setImage("MriPics\\mri2.jpg"));
-    }
-         
     public void analyzeImage(ActionEvent event) {
-    	mainImage.setImage(fileManager.setImage(Analyze.analyzeImage("MriPics\\mri2.jpg")));
+    	if (dir != null) {
+    		mainImage.setImage(fileManager.setImage(Analyze.analyzeImage(dir + sep + "mri2.jpg")));
+    	}
+    	else {
+    		System.out.println("No directory selected!");
+    	}
     }
     
     @Override
@@ -61,8 +96,8 @@ public class FXMLDocumentController extends AnchorPane implements Initializable 
     	
     }    
     
-   	void setApp(MRIPrototype aThis) {
-	    this.application = aThis;
+   	void setApp(MRIPrototype application) {
+	    this.application = application;
 	    throw new UnsupportedOperationException("Not supported yet.");
     }
 }
