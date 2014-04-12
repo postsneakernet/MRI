1467800717399ebe8643decbfc6692e7eb8867bd
bpaxson62 <bpaxson62@gmail.com>
2014-04-11 21:39:31 -0400

    MAde graph bikg

--- MRI_Scanner/src/analysis/Graphing.java
+++ MRI_Scanner/src/analysis/Graphing.java
@@ -1,16 +1,76 @@
 package analysis;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import com.xeiam.xchart.BitmapEncoder;
+import com.xeiam.xchart.Chart;
+
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 
 public class Graphing {
 
-	
-	
-	
-//	Image createGraph(){
-//		
-//	}
-	
-	
+	Image createGraph(ArrayList<double[]> myData) {
+		BufferedImage img = null;
+
+		ArrayList<double[]> abc = new ArrayList<double[]>();
+
+		for (int i = 1; i < 30.0; i++) {
+			double insert[] = new double[2];
+			insert[0] = i;
+			insert[1] = Math.log(i) / Math.log(2);
+			abc.add(i - 1, insert);
+
+		}
+
+//		double[] datVolume = new double[abc.size()];
+//		double[] myMonths = new double[abc.size()];
+//
+//		for (int i = 0; i < abc.size(); i++) {
+//
+//			myMonths[i] = abc.get(i)[0];
+//
+//			datVolume[i] = abc.get(i)[1];
+//
+//		}
+		
+		double[] datVolume = new double[myData.size()];
+		double[] myMonths = new double[myData.size()];
+
+		for (int i = 0; i < myData.size(); i++) {
+
+			myMonths[i] = myData.get(i)[0];
+
+			datVolume[i] = myData.get(i)[1];
+
+		}
+
+		
+		Chart chart = new Chart(555, 333);
+		chart.addSeries("Volume", myMonths, datVolume);
+		chart.setChartTitle("Tumor Mapping");
+		chart.setXAxisTitle("Months");
+		chart.setYAxisTitle("Volume");
+
+		try {
+
+			byte[] imageInBytes = BitmapEncoder.getPNGBytes(chart);
+
+			InputStream in = new ByteArrayInputStream(imageInBytes);
+			img = ImageIO.read(in);
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		return SwingFXUtils.toFXImage(img, null);
+	}
 
 }
