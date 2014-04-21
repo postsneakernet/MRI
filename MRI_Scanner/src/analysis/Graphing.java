20dc388b4a581bc3ee3485a60ba89c91ce886e50
Brian <Brian@10.100.55.220>
2014-04-21 15:00:11 -0400

    This is a triumph on the graph regarding fake alpha beta gamma

--- MRI_Scanner/src/analysis/Graphing.java
+++ MRI_Scanner/src/analysis/Graphing.java
@@ -3,6 +3,7 @@ package analysis;
 import com.xeiam.xchart.BitmapEncoder;
 import com.xeiam.xchart.Chart;
 import com.xeiam.xchart.Series;
+import com.xeiam.xchart.SeriesLineStyle;
 import com.xeiam.xchart.SeriesMarker;
 
 import java.awt.Color;
@@ -19,98 +20,85 @@ import javafx.scene.image.Image;
 import javax.imageio.ImageIO;
 
 public class Graphing {
-	
-	//final double pixelToCm = .0000185254;
-	 double alpha = 1.0;
-	 double beta = 1.0;
-	 double gamma = 1.0;
-//	 static double p = .75;
-//	 static double c = .1;
 
-	
+	// final double pixelToCm = .0000185254;
+	static double alpha = 1.5; //1.5;
+	static double beta = 1.5;//1.5;
+	static double gamma = 1;
+	// static double p = .75;
+	// static double c = .1;
+
 	final double pixelToCm = .0000185254;
-	
-	public double calcXCoord(double myVolume, double prevVolume){
-		//V1 = alphaV^p(beta-gammaVsub1^(1-p))-c
-		
-		
-		
-		
-		
+
+	public double calcXCoord(double myVolume, double prevVolume) {
+		// V1 = alphaV^p(beta-gammaVsub1^(1-p))-c
+
 		return 0.0;
 	}
 
 	public static Image createGraph(List<Double> myData, double p, double c) {
 		BufferedImage img = null;
 
-//		ArrayList<double[]> abc = new ArrayList<double[]>();
-//
-//		for (int i = 1; i < 30.0; i++) {
-//			double insert[] = new double[2];
-//			insert[0] = i;
-//			insert[1] = Math.log(i) / Math.log(2);
-//			abc.add(i - 1, insert);
-//
-//		}
+		// ArrayList<double[]> abc = new ArrayList<double[]>();
+		//
+		// for (int i = 1; i < 30.0; i++) {
+		// double insert[] = new double[2];
+		// insert[0] = i;
+		// insert[1] = Math.log(i) / Math.log(2);
+		// abc.add(i - 1, insert);
+		//
+		// }
 
-//		double[] datVolume = new double[abc.size()];
-//		double[] myMonths = new double[abc.size()];
-//
-//		for (int i = 0; i < abc.size(); i++) {
-//
-//			myMonths[i] = abc.get(i)[0];
-//
-//			datVolume[i] = abc.get(i)[1];
-//
-//		}
-		
-		
-		
-		
-		
-		
-		double[] datVolume = new double[myData.size()];
-		double[] myMonths = new double[myData.size()];
+		// double[] datVolume = new double[abc.size()];
+		// double[] myMonths = new double[abc.size()];
+		//
+		// for (int i = 0; i < abc.size(); i++) {
+		//
+		// myMonths[i] = abc.get(i)[0];
+		//
+		// datVolume[i] = abc.get(i)[1];
+		//
+		// }
 
+		double[] datVolume = new double[myData.size()+1];
+		double[] myMonths = new double[myData.size()+1];
+		datVolume[0] = 0;
+		myMonths[0] = 0;
+		
 		for (int i = 0; i < myData.size(); i++) {
-
-			myMonths[i] = i;
-
-			datVolume[i] = myData.get(i);
-
+//			if (i == 0) {
+//				myMonths[i] = 0;
+//				datVolume[i] = 0;
+//			}else{
+				myMonths[i+1] = i+1;
+				datVolume[i+1] = myData.get(i);
+			//}
 		}
 
-		
 		Chart chart = new Chart(564, 372);
 		chart.addSeries("Volume", myMonths, datVolume);
 		chart.setChartTitle("Tumor Mapping");
 		chart.setXAxisTitle("Months");
 		chart.setYAxisTitle("Volume");
-		
-		
-		
-		 double uSeriesX[] = new double[61];
-		 double uSeriesY[] = new double[61];
-		 double V = 0;
-		 int i = 0;
-		 for(double u = 0.0; u<6; u+=0.1){
-			// System.out.println(p);
-			 V = (Math.pow(u, p))*(1-Math.pow(u, 1-p)) - c;
-			 //System.out.println(i);
-			 uSeriesY[i] = V;
-			 uSeriesX[i] = u;
-			 
-			 i++;
-		 }
-		 
-		
+		chart.getSeriesMap().get("Volume").setLineStyle(SeriesLineStyle.NONE);
 
-		 Series series2 = chart.addSeries("another", uSeriesX, uSeriesY);
-		 series2.setMarkerColor(Color.BLUE);
-		 series2.setMarker(SeriesMarker.CIRCLE);
-		
-		
-		
+		double uSeriesX[] = new double[61];
+		double uSeriesY[] = new double[61];
+		double V = 0;
+		int i = 0;
+		for (double u = 0.0; u < 6; u += 0.1) {
+			// System.out.println(p);
+			V = (alpha) * (Math.pow(u, p))
+					* (beta - (gamma) * Math.pow(u, 1 - p)) - c;
+			// System.out.println(i);
+			uSeriesY[i] = V;
+			uSeriesX[i] = u;
+
+			i++;
+		}
+
+		Series series2 = chart.addSeries("GUE", uSeriesX, uSeriesY);
+		series2.setMarker(SeriesMarker.NONE);
 
 		try {
 
@@ -126,7 +114,7 @@ public class Graphing {
 
 		return SwingFXUtils.toFXImage(img, null);
 	}
-	
+
 	public double convertToCm(double myVolume) {
 		return myVolume * this.pixelToCm;
 	}
